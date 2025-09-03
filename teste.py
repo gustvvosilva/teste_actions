825ff1d80389cafad14a66f95c374e0e1a2bd21a
Gustavo Silva <gustavosilvadeoliveira02@gmail.com>
2025-09-03 19:22:12 -0300

    22222

--- teste.py
+++ teste.py
@@ -1,18 +1,18 @@
 import pytest
-import meu_codigo
+import calc
 
 def test_soma():
-    assert meu_codigo.soma(2, 3) == 5
+    assert calc.soma(2, 3) == 5
 
 def test_subtracao():
-    assert meu_codigo.subtracao(10, 5) == 5
+    assert calc.subtracao(10, 5) == 5
 
 def test_multiplicacao():
-    assert meu_codigo.multiplicacao(4, 3) == 12
+    assert calc.multiplicacao(4, 3) == 12
 
 def test_divisao():
-    assert meu_codigo.divisao(10, 2) == 5
+    assert calc.divisao(10, 2) == 5
 
 def test_divisao_por_zero():
     with pytest.raises(ValueError):
-        meu_codigo.divisao(5, 0)
+        calc.divisao(5, 0)
